604082bb9389d246b504f0721c71f4b3a16e8d2e
Zhongwei Li <li@Zhongweis-Air.lan>
2022-05-12 22:42:49 -0400

    fix bugs with edit campus page

--- src/store/thunks.js
+++ src/store/thunks.js
@@ -39,10 +39,6 @@ export const addCampusThunk = (campus) => async (dispatch) => {  // The THUNK
 // THUNK CREATOR:
 export const editCampusThunk = campus => async dispatch => {  // The THUNK
   try {
-    // Remove a field if its not being used to update (empty field)
-    for (const field in campus) {
-      if (campus[field] === "" || campus[field] === null) delete campus[field];
-    }
     // API "put" call to update campus (based on "id" and "student" object's data) from database
     let updatedCampus = await axios.put(`/api/campuses/${campus.id}`, campus); 
     // Update successful so change state with dispatch
@@ -110,10 +106,6 @@ export const deleteStudentThunk = studentId => async dispatch => {  // The THUNK
 // THUNK CREATOR:
 export const editStudentThunk = student => async dispatch => {  // The THUNK
   try {
-    // Remove a field if its not being used to update (empty field)
-    for (const field in student) {
-      if (student[field] === "" || student[field] === null) delete student[field];
-    }
     // API "put" call to update student (based on "id" and "student" object's data) from database
     let updatedStudent = await axios.put(`/api/students/${student.id}`, student); 
     // Update successful so change state with dispatch
