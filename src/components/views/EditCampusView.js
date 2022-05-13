eff0cd6b71f7fd7ef63cec660ec8a09e75b0af73
Zhongwei Li <li@Zhongweis-Air.lan>
2022-05-13 18:18:07 -0400

    remove unneccesary import

--- src/components/views/EditCampusView.js
+++ src/components/views/EditCampusView.js
@@ -8,8 +8,6 @@ import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 
-import PropTypes from "prop-types";
-
 // Create styling for the input form
 const useStyles = makeStyles( () => ({
   formContainer:{  
